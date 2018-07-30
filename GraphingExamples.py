8e1f845388ac8c9ddc75bdae3b3f906539424266
Kkbulmer <41701842+Kkbulmer@users.noreply.github.com>
2018-07-30 10:15:47 -0400

    Update GraphingExamples.py

--- GraphingExamples.py
+++ GraphingExamples.py
@@ -9,13 +9,19 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
+#loads a pre-loaded dataframe caleld tips.
 df = sns.load_dataset("tips")
+#groups the values containing a day into 1 and counts the amount of that day there are.
 groupedvalues=df.groupby('day').sum().reset_index()
 
-pal = sns.color_palette("Greens_d", len(groupedvalues))
+#creates a palette based off of length of days in the variable
+pal = sns.color_palette("Greens_d", len(groupedvalues))\
+#sorts the values by the total bill
 rank = groupedvalues["total_bill"].argsort().argsort() 
+#create a bargraph with the days at the x, tip price as the y, using the grouped data we created.
 g=sns.barplot(x='day',y='tip',data=groupedvalues, palette=np.array(pal[::-1])[rank])
 
+#labels the prices for each bar plot
 for index, row in groupedvalues.iterrows():
     g.text(row.name,row.tip, round(row.total_bill,2), color='black', ha="center")
 
